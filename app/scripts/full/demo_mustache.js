e5beae40740b8a546d331afe698e326cac8e489e
jsbhb <306494983@qq.com>
2017-05-22 14:42:43 +0800

    demo commit

--- app/scripts/full/demo_mustache.js
+++ app/scripts/full/demo_mustache.js
@@ -1,25 +1,1 @@
-/**
- * Created by linpengteng on 2017/5/15.
- */
-define(["can"], function(can){
-    var data = {
-        "user": {
-            name: "林鹏腾",
-            description: "欢迎使用Canjs!"
-        },
-        "info": [
-            "require(js模块化技术)",
-            "jquery(jquery框架)",
-            "can.jquery(依赖于jquery的canjs)",
-            "mustache(前端模板)"
-        ],
-        author: "linpengteng"
-    };
-
-    $("#demo_header").html(can.view("mustache_header.mustache",data));
-    $("#demo_center").html(can.view("mustache_center.mustache",data));
-    $("#demo_tfoot").html(can.view("mustache_tfoot.mustache",data));
-
-
-
-});
+/** * Created by linpengteng on 2017/5/15. */define(["can"], function(can){    var data = {        "user": {            name: "林鹏腾",            description: "欢迎使用Canjs!"        },        "info": [            "require(js模块化技术)",            "jquery(jquery框架)",            "can.jquery(依赖于jquery的canjs)",            "mustache(前端模板)"        ],        author: "linpengteng"    };    $("#demo_header").html(can.view("mustache_header.mustache",data));    $("#demo_center").html(can.view("mustache_center.mustache",data));    $("#demo_tfoot").html(can.view("mustache_tfoot.mustache",data));});
