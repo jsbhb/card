30e0fdf7300f932b741a2d3ba7005dc866d1434e
linpengteng <1990002080@qq.com>
2017-05-27 08:05:10 +0800

    update

--- app/scripts/.config/require.js
+++ app/scripts/.config/require.js
@@ -32,23 +32,23 @@ requirejs.config({
             "comm.query.city":  "app/scripts/comm/query-city",
             "comm.query.nav":   "app/scripts/comm/query-nav",
 
-            //模板组件
+            //组件(模板/插件)
             "component.page.header.top":     "app/scripts/component/page-header-top",
             "component.page.header.center":  "app/scripts/component/page-header-center",
             "component.page.body.nav":       "app/scripts/component/page-body-nav",
 
-            //页面(业务)
+            //业务(页面/数据)
             "control.page":                "app/scripts/control/page",
             "control.page.header.top":     "app/scripts/control/page-header-top",
             "control.page.header.center":  "app/scripts/control/page-header-center",
             "control.page.body.nav":       "app/scripts/control/page-body-nav",
 
-            //模拟REST
+            //模拟(REST)
             "fixture.data":  "app/scripts/fixture/fixture-data",
             "fixture.test":  "app/scripts/fixture/fixture-test",
 
         // -------------------- app/templates --------------------------
-            //页面模板
+            //模板(页面/组件)
             "templates.page.header.top": "app/templates/page-header-top",
             "templates.page.header.center": "app/templates/page-header-center",
             "templates.page.body.nav": "app/templates/page-body-nav"
