11ffdb8ade5bf0590968f3495556de4a1b1cf17f
linpengteng <1990002080@qq.com>
2017-06-05 05:05:14 +0800

    优化页面

--- app/scripts/component/page-nav-1.js
+++ app/scripts/component/page-nav-1.js
@@ -27,8 +27,8 @@ define([
                     $(".contType>ul").removeClass("hover");
                     $(".contType>ul>li").removeClass("hover");
                     $(".contType>ul>li .showContItemList").css("visibility", "hidden");
-                    if(!$element.parent().hasClass("showed") && !this.isShow){
-                        $element.parent().css("overflow", "hidden");
+                    if(!$element.hasClass("showed") && !this.isShow){
+                        $element.css("overflow", "hidden");
                     }
                 }else{
                     this.timer=setTimeout(function(){
@@ -42,8 +42,13 @@ define([
                 $(element).css("overflow", "visible");
                 this.scope.isShow = true;
             },
-            ".contType mouseleave": function(){
+            ".contType mouseleave": function(element){
                 this.scope.isShow = false;
+                var that = this;
+                var $element = element;
+                this.scope.timer=setTimeout(function(){
+                    that.scope.getWIdth($element);
+                },300);
             },
             ".contType>ul mouseenter": function(element){
                 clearTimeout(this.scope.timer);
@@ -51,7 +56,7 @@ define([
             },
             ".contType>ul mouseleave": function(element){
                 var that = this;
-                var $element = element;
+                var $element = element.parent();
                 this.scope.timer=setTimeout(function(){
                     that.scope.getWIdth($element);
                 },300);
