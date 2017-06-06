45e14bafd1788c08e5f1b132da7eb2211d4d1f88
linpengteng <1990002080@qq.com>
2017-06-06 14:23:05 +0800

    优化页面

--- app/scripts/component/page-nav-1.js
+++ app/scripts/component/page-nav-1.js
@@ -16,56 +16,67 @@ define([
         tag: "page-nav-1",
         template: can.view("templates.page.nav.1.mustache"),
         helpers: {
+            setULHeight: function(cont){
+                this.attr("ulHeight", 50*cont.length+"px");
+            }
         },
         scope: {
-            isShow: false,
-            timer: null,
-            getWIdth: function($element){
+            ul_timer: null,
+            contType_timer: null,
+            ulHeight: "0px",
+            ul_mouseleave_end: true,
+            ul_mouseleave: function(){
                 var that = this;
-                var width = parseFloat($element.find(".showContItemList:first").css("width"));
+                var width = parseFloat($(".contType>ul").find(".showContItemList:first").css("width"));
                 if(width<2){
+                    that.ul_mouseleave_end = true;
                     $(".contType>ul").removeClass("hover");
                     $(".contType>ul>li").removeClass("hover");
                     $(".contType>ul>li .showContItemList").css("visibility", "hidden");
-                    if(!$element.hasClass("showed") && !this.isShow){
-                        $element.css("overflow", "hidden");
-                    }
                 }else{
-                    this.timer=setTimeout(function(){
-                        that.getWIdth($element);
-                    },200);
+                    that.ul_mouseleave_end = false;
+                    this.ul_timer=setTimeout(function(){ that.ul_mouseleave(); },200);
+                }
+            },
+            contType_mouseleave:function(){
+                var that = this;
+                if(that.ul_mouseleave_end && !$(".contType").hasClass("showed")){
+                    $(".contType>ul").css("overflow", "hidden");
+                    $(".contType>ul").stop(true, true).animate({height: "0px"}, 1000);
+                }else if(!$(".contType").hasClass("showed")){
+                    this.contType_timer=setTimeout(function(){ that.contType_mouseleave(); },200);
                 }
             }
         },
         events: {
             ".contType mouseenter": function(element){
-                $(element).css("overflow", "visible");
-                this.scope.isShow = true;
-            },
-            ".contType mouseleave": function(element){
-                this.scope.isShow = false;
                 var that = this;
-                var $element = element;
-                this.scope.timer=setTimeout(function(){
-                    that.scope.getWIdth($element);
-                },300);
+                $(".contType>ul").stop(true, true);
+                clearTimeout(that.scope.contType_timer);
+                $(".contType>ul").css({height: this.scope.ulHeight, overflow:"visible"});
             },
             ".contType>ul mouseenter": function(element){
-                clearTimeout(this.scope.timer);
-                $(element).addClass("hover");
-            },
-            ".contType>ul mouseleave": function(element){
                 var that = this;
-                var $element = element.parent();
-                this.scope.timer=setTimeout(function(){
-                    that.scope.getWIdth($element);
-                },200);
+                clearTimeout(that.scope.ul_timer);
+                $(element).addClass("hover");
             },
             ".contType>ul>li mouseenter": function(element){
                 $(".contType>ul>li").removeClass("hover");
                 $(".contType>ul>li .showContItemList").css("visibility", "hidden");
                 $(element).addClass("hover");
                 $(element).find(".showContItemList").css("visibility", "visible");
+            },
+            ".contType>ul mouseleave": function(){
+                var that = this;
+                this.scope.timer=setTimeout(function(){
+                    that.scope.ul_mouseleave();
+                },200);
+            },
+            ".contType mouseleave": function(){
+                var that = this;
+                this.scope.timer=setTimeout(function(){
+                    that.scope.contType_mouseleave();
+                },200);
             }
         }
     })
